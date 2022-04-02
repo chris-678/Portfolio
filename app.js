1cba548c961e34b13149d70df20672a103ff382c
izuchrisa <izuchrisa@gmail.com>
2022-04-02 15:39:36 +0100

    update version of my profile site

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact");
+});
+
+app.get("/profile", function(req, res){
+  res.render("profile");
+});
+
+
+
+app.listen(process.env.PORT || 3001, function(){
+  console.log("Server started on port 3001");
+});
